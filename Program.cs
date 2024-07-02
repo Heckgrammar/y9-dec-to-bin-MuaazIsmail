b0717a0bb2d43b6fab6e74c45bc4ed0fc46c6b27
MuaazIsmail <160234344+MuaazIsmail@users.noreply.github.com>
2024-07-02 14:43:45 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,23 +10,24 @@ namespace Y9_DEC_TO_BIN_SKELETON
     {
         static void Main(string[] args)
         {
-            //STARTER: Practice using breakpoints and the watch window (F8 to single-step, F11 to start in single step mode)
-            int myInt = 0; //just for testing single stepping
-            string myString = "12"; //watch me being cast from string to int
-            int myStringAsInt = Convert.ToInt32(myString); //watch me cast from string to int
-
-            //MAIN:  NUMBER CONVERSION PROGRAM
-
-            //CODE GOES HERE
+            Console.WriteLine("Enter an integer you want to convert to binary");
+            int userNum = Int32.Parse(Console.ReadLine());
+            Console.WriteLine(userNum + " is " + (numberConversion(userNum, 2)) + " in binary");
         }
 
         //static void means the function will not return a value so it does not need a data type 
         //...this function DOES return a value so the method must have a data type
         static string numberConversion(int number, int numberbase)
         {
-            //CODE GOES HERE
-
-            return result; //REMOVE THE RED LINE!
+            int remainder = 0;
+            string result = "";
+            while (number > 0)
+            {
+                remainder = number % numberbase;
+                number = number / numberbase;
+                result = remainder + result;
+            }
+            return result;
         }
     }
 }
